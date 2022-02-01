08dd588afcae00b861a93334550b69fac102f74e
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-31 16:07:10 -0800

    pass Triangle constructor test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import Triangle from "./triangle.js";
+import $ from "jquery";
+
 $(document).ready(function () {
   $("#triangle-checker-form").submit(function (event) {
     event.preventDefault();
+    debugger;
     const length1 = $("#length1").val();
     const length2 = $("#length2").val();
     const length3 = $("#length3").val();
